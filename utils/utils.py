cdec88862e49185897050b36045e4d2e892dea45
Jathurshan0330 <52663918+Jathurshan0330@users.noreply.github.com>
2022-02-19 18:28:55 +0530

    create modules for model and average metrics

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,6 @@
+import torch
+import torch.nn as nn
+
 class AverageMeter(object):
     """Computes and stores the average and current value"""
     def __init__(self):
@@ -14,3 +17,14 @@ class AverageMeter(object):
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count if self.count != 0 else 0
+
+
+def img_net_normalize(x):
+    return torch.cat(((x-0.485)/0.229, (x-0.456)/0.224, (x-0.406)/0.225), 1)
+
+def weights_init(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        torch.nn.init.constant_(m.bias, 0)
