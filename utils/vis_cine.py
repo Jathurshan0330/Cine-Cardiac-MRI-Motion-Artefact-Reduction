2fe287fbe9a9723a2e04d08bc7bcc4910fe66e54
Jathurshan0330 <52663918+Jathurshan0330@users.noreply.github.com>
2022-02-19 17:56:51 +0530

    create libraries for datasets and cine cardia visualization

--- utils/vis_cine.py
+++ utils/vis_cine.py
@@ -3,6 +3,7 @@ from ipywidgets import  interactive, IntSlider
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style('darkgrid')
+from matplotlib.animation import FuncAnimation
 from IPython import display
 
 
